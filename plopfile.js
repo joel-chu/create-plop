9b2bf4445853f2a9044f08b7a07928122f98c2b8
Joel Chu <joelckchu@gmail.com>
2022-07-07 15:58:33 +0800

    setting up several debug options

--- plopfile.js
+++ plopfile.js
@@ -1,29 +1,47 @@
 // Internal plopfile
 // plop file for all the commands
 import { join } from 'node:path'
-import fs from 'fs-extra'
+import fsx from 'fs-extra'
 import debugFn from 'debug'
 import { getDirname } from '@jsonql/utils/dist/get-dirname.js'
-import { importPlopfile } from './src/import-plopfile.mjs'
+import { PKG_FILE } from './src/helpers/constants.mjs'
+// import { importPlopfile } from './src/import-plopfile.mjs'
 // import { spaceInValue } from './src/common.mjs'
 const __dirname = getDirname(import.meta.url)
 const debug = debugFn('create-plop:plopfile')
-const tplDir = join(__dirname, 'templates')
-const isTest = process.env.NODE_ENV === 'test'
-const destDir = isTest ? join(__dirname, 'tests', 'fixtures') : process.cwd()
-const projectRoot = isTest ? __dirname : process.cwd()
+const tplDir = join(__dirname, 'src', 'templates')
+// const isTest = process.env.NODE_ENV === 'test'
+// const destDir = isTest ? join(__dirname, 'tests', 'fixtures') : process.cwd()
+
+// const projectRoot = isTest ? __dirname : process.cwd()
 // @TODO scan the dest to see if there is any plopfile.js
 // if yes then import it here and run within the method also
+/*
+Process:
+- read the dest directory and find the package.json
+- the project name will be re-use, also check if they have the target deps installed, then we can skip some of the questions
+- After: copy the required template, generate a user manual
 
+options:
+- Lang: TS / JS
+- Framework: Vue 2 / 3
+- Feature: SPA / SSR (TBC is there any different?)
+- State machine: VueX (Vue 2, default to 3, 3 default to 4) or Pinia
+- anything else?
+*/
 // export
 export default function (
   /** @type {import('plop').NodePlopAPI} */
   plop
 ) {
+  const dest = plop.getDestBasePath()
+  debug('dest', dest)
+  const pkgJson = fsx.readJsonSync(join(dest, PKG_FILE))
+  debug('pkgJson', pkgJson)
   // create the generator
   // @TODO add more framework support next
-  plop.setGenerator('base', {
-    description: 'Hello world',
+  plop.setGenerator('createPlop', {
+    description: 'create-plop main',
     prompts: [{
       type: 'input',
       name: 'name',
@@ -81,8 +99,7 @@ export default function (
     return 'Setup completed, now please run "npm install" then run "npm run dev"'
   })
 
-
   // next we will try to import plopfile that is written by the developer
   // then import it here
-  importPlopfile(projectRoot, plop)
+  // importPlopfile(projectRoot, plop)
 }
--- src/helpers/constants.mjs
+++ src/helpers/constants.mjs
@@ -1,3 +1,4 @@
 // where we store the templates etc on the project
 export const BASE_DIR_NAME = '.plop'
 export const CONFIG_FILE_NAME = 'create-plop.json'
+export const PKG_FILE = 'packages.json'
