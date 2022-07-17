9a8df22fc4d1565988d49010ab0af3096218e0a7
Joel Chu <joelckchu@gmail.com>
2022-07-17 12:51:13 +0800

    move everything out into the tmpGenerator

--- plopfile.js
+++ plopfile.js
@@ -3,11 +3,11 @@
 import { join } from 'node:path'
 import fsx from 'fs-extra'
 import debugFn from 'debug'
-import { extend } from '@jsonql/utils'
+// import generators
+import tmpGenerator from './src/generators/tmp.mjs'
+
 import { getDirname } from '@jsonql/utils/dist/get-dirname.js'
-import { checkPkgDeps } from './src/helpers/check-pkg-deps.mjs'
-import { PKG_FILE, USERGUIDE } from './src/helpers/constants.mjs'
-import { tmpContinue } from './src/plop/prompts.mjs'
+import { PKG_FILE } from './src/helpers/constants.mjs'
 // import { importPlopfile } from './src/import-plopfile.mjs'
 // import { spaceInValue } from './src/common.mjs'
 const __dirname = getDirname(import.meta.url)
@@ -43,44 +43,10 @@ export default function (
   debug('dest', dest)
   const pkgJsonFile = join(dest, PKG_FILE)
   const pkgJson = fsx.existsSync(pkgJsonFile) ? fsx.readJsonSync(pkgJsonFile) : {}
-  const deps = checkPkgDeps(pkgJson)
-  if (deps.vue && deps.vue !== 2) {
-    throw new Error(`We only support Vue.2 at the moment (you have v.${deps.vue}). Please check back in later release`)
-  }
+  const config = { dest, pkgJsonFile, pkgJson, tplDir, ourPkgJson, __dirname }
   // debug('deps', deps)
   // main
-  plop.setGenerator('createPlop', {
-    description: 'create-plop main',
-    prompts: [
-      tmpContinue
-    ],
-    actions: function (answers) {
-      debug('actions answers', answers)
-      const { proceed } = answers
-      if (!proceed) {
-        return [function () { return 'Bye bye' }]
-      }
-      // copy over the whole folder content
-      return [
-        async function copyFiles () {
-          return await fsx.copy(join(tplDir, 'vue2'), dest)
-        },
-        async function updatePackageJson () {
-          // create a copy first
-          await fsx.copy(pkgJsonFile, pkgJsonFile.replace('.json', '-org.json'))
-
-          pkgJson.dependencies = extend(pkgJson.dependencies, { plop: ourPkgJson.dependencies.plop })
-          pkgJson.scripts = extend(pkgJson.scripts, { plop: 'plop' })
-
-          return fsx.writeJson(pkgJsonFile, pkgJson, { spaces: 2 })
-        },
-        async function copyUserGuide () {
-          return fsx.copy(join(tplDir, USERGUIDE), join(dest, USERGUIDE))
-            .then(() => 'All done! Just run `npm run plop`')
-        }
-      ]
-    }
-  })
+  tmpGenerator(config)(plop)
 
   // next we will try to import plopfile that is written by the developer
   // then import it here
--- src/generators/setup.mjs
+++ src/generators/setup.mjs
@@ -1,5 +1,12 @@
 // setup generator create the init setup and the .create-plop.config.json file
+/*
+What do we need to know:
+1. framework (only Vue at the moment) stub this with a message
+2. version (2 or 3)
+3. Using which store
+4. What test framework (Support Jest, ava, and Vitest soon follow)
 
+*/
 export default function setupGenerator (config = {}) {
   return function (plop) {
     plop.setGenerator('initGenerator', {
