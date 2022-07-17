353a416e4aceb9adb87eca0db9f312fe1a35127e
Joel Chu <joelckchu@gmail.com>
2022-07-17 21:08:03 +0800

    add description

--- src/generators/create-generator.mjs
+++ src/generators/create-generator.mjs
@@ -9,11 +9,17 @@ export default function createGenerator (plop, config) {
     prompts: [{
       type: 'input',
       name: 'name'
+    }, {
+      type: 'input',
+      name: 'description',
+      default: function (answers) {
+        return `${answers.name} generator description`
+      }
     }],
     actions: [{
       type: 'add',
       path: join(__dirname, 'src', 'generators', '{{dashCase name}}.mjs'),
-      template: join(__dirname, 'src', 'templates', 'plop', 'generator.hbs')
+      templateFile: join(__dirname, 'src', 'templates', 'plop', 'generator.hbs')
     }]
   })
 }
